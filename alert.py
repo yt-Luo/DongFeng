92d87ef884a090e3fce63331c7138d6e6ab18b8a
ytLuo <73994810+yt-Luo@users.noreply.github.com>
2021-05-26 13:21:20 +0800

    upload
    改變數名稱
    增加read_file
    寄件人收件人資訊均用txt紀錄

--- alert.py
+++ alert.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 import pandas as pd
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText#專門傳送正文
-from email.mime.image import MIMEImage
+from email.mime.text import MIMEText #專門傳送正文
 import smtplib
 import datetime
-
 
 #讀excel
 def load_data(file_name):
@@ -21,14 +18,14 @@ def load_data(file_name):
 def data_process(df, df1):
     tmp = df.append(df1)
     tmp.drop_duplicates(keep=False, inplace=True)
-    df2=pd.DataFrame(tmp)
+    all_diff =pd.DataFrame(tmp)
 
-    last = df2.drop_duplicates(subset=['pk'],keep='last')
-    first = df2.drop_duplicates(subset=['pk'],keep='first')
-    dupl = df2.drop_duplicates(subset=['pk'],keep=False)
+    last = all_diff.drop_duplicates(subset=['pk'],keep='last')
+    first = all_diff.drop_duplicates(subset=['pk'],keep='first')
+    pk_diff = all_diff.drop_duplicates(subset=['pk'],keep=False)
 
-    new = last.append(dupl).drop_duplicates(subset=['pk'],keep=False)
-    old = first.append(dupl).drop_duplicates(subset=['pk'],keep=False)
+    new = last.append(pk_diff).drop_duplicates(subset=['pk'],keep=False)
+    old = first.append(pk_diff).drop_duplicates(subset=['pk'],keep=False)
 
     final = new.merge(old,on = ['pk','OEB01','OEB03','OEB15','OEA02','OEA14'], suffixes=('_new','_old') )
     final = final[['OEA14','OEA02','OEB01','OEB03','OEB15','OEB16_old','OEB16_new']]
@@ -44,31 +41,30 @@ def data_process(df, df1):
         return 1, df_html
 
 def read_file(file):
-    with open(file,'r') as f:
-            content_list = f.read()
+    with open(file,'r',encoding="utf-8") as f:
+            content_list = f.read().split('\n')
     return content_list
 
 def SendMail(df_html):
     #print('start to send mail...')
-    sender = "hl94vul3h6@gmail.com" #step1:setup sender gmail,ex:"Fene1977@superrito.com"
-    password = "eeecnzvlhavhksyb" #step2:setup sender gmail password
-    recipients = read_file('收件人.txt') #step3:setup recipients mail
+    emails = read_file('email.txt') #讀email資料
+    sender = emails[0] #step1:setup sender gmail,ex:"Fene1977@superrito.com"
+    password = emails[1] #step2:setup sender gmail password
+    recipients = emails[2] #step3:setup recipients mail
     today_date = datetime.date.today()
     sub = today_date.strftime("%m/%d") + "訂單交期更改通知" #step4:setup your subject
-    
-    #單個收件人,ex:recipients = 'Fene1977@dayrep.com'  
-    #多個收件人,ex:"Hinte1969@jourrapide.com,Fene1977@rhyta.com,Fene1977@teleworm.us"
     
     outer = MIMEMultipart()
     outer['From'] = sender #step:setup sender gmail
     outer['To'] = recipients #step:setup recipient mail
-    #outer["Cc"]="cc mail" #step:setup cc mail
+    #outer["Cc"] = cc_mail #step:setup cc mail
     outer['Subject'] = sub #step:setup your subject
 
     #設定純文字資訊
     plainText = "偵測到訂單排定交貨日更改，請確認以下訂單："
     msgText = MIMEText(plainText, 'plain', 'utf-8')
     outer.attach(msgText)
+    
     #設定HTML資訊
     htmlText = df_html #step7:edit your mail content
     msgText = MIMEText(htmlText, 'html', 'utf-8')
@@ -76,7 +72,6 @@ def SendMail(df_html):
 
     mailBody = outer.as_string()
     #-----------------------------------------------------------------------
-    
     # 寄送EMAIL
     try:
         with smtplib.SMTP(host="smtp.gmail.com", port="587") as s: #send webservice to gmail smtp socket
@@ -96,7 +91,7 @@ def main():
     flag, mail_content = data_process(yesterday, today)
 
     if flag == 0:
-        print("未更改")
+        print("交期未更改")
     else:
         SendMail(mail_content)
         print("交期已被更改")
